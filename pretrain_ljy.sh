c34e0e15d40fd71e76fa6e8b37af799bc02092bc
DukeEnglish <ljyduke@gmail.com>
2020-03-09 15:33:18 +0800

    demo for pretrain

--- pretrain_ljy.sh
+++ pretrain_ljy.sh
@@ -16,8 +16,8 @@ do
   printf -v l "%07d" $i
   echo $l                                 # original: bert_base_128_vocab8k
   python3 create_pretraining_data.py  \
-    --input_file=gs://clue_pretrain_corpus/raw_txt_corpus/train/clue_pretrain_$l.txt \
-    --output_file=gs://clue_pretrain_corpus/tfrecords/bert_base_128_c5_vocab8k/clue_pretrain128_$i.tfrecord \
+    --input_file=gs://clue_storage/corpus2020/clue_pretrain_train_000$l.zip   \
+    --output_file=gs://clue_storage/clue_pretrain_corpus/tfrecords/bert_base_128_c5_vocab8k/clue_pretrain128_$i.tfrecord \
     --vocab_file=./bert_base_dir/chinese_L-12_H-768_A-12/vocab_clue.txt  \
     --t2s=True  \
     --do_lower_case=True \
