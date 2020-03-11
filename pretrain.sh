6ffb888000ea02820c7734422c309aeedecee1ec
DukeEnglish <ljyduke@gmail.com>
2020-03-11 08:59:38 +0800

    modify para

--- pretrain.sh
+++ pretrain.sh
@@ -1,8 +1,4 @@
 
-
-
-
-
 # --input_file=gs://clue_storage/clue_pretrain_corpus/raw_txt_corpus/train/clue_pretrain_$l.txt   \
 # --output_file=gs://clue_storage/clue_pretrain_corpus/tfrecords/bert_base_128_c5_vocab8k/clue_pretrain128_$i.tfrecord \
 # --vocab_file=./RoBERTa-tiny-clue/vocab.txt  \
@@ -13,7 +9,7 @@ OUT_PUT_DIR=gs://clue_storage/clue_pretrain_corpus/pretraining_output_310_100tes
 TPU_IP=10.240.1.2
 # bert_base/vocab_clue.txt
 
-python3 run_pretraining.py \
+nohup python3 run_pretraining.py \
   --input_file=$INPUT_DIR \
   --output_dir=$OUT_PUT_DIR \
   --do_train=True \
@@ -22,12 +18,11 @@ python3 run_pretraining.py \
   --train_batch_size=4096 \
   --max_seq_length=512 \
   --max_predictions_per_seq=51 \
-  --num_train_steps=1000 \
-  --num_warmup_steps=10 \
-  --learning_rate=2e-5 \
-  --save_checkpoints_steps=200 \
-  --num_tpu_cores=8 --use_tpu=True --tpu_name=grpc://$TPU_IP:8470
-
+  --num_train_steps=12500 \
+  --num_warmup_steps=1000 \
+  --learning_rate=0.00176  \
+  --save_checkpoints_steps=2000 \
+  --num_tpu_cores=8 --use_tpu=True --tpu_name=grpc://$TPU_IP:8470 &
 
 
 # python3 run_pretraining.py \
